86d6818285564ed3f3c173322385438c11caa16e
Maximiliano Bogoljubskij <mbogoljubsklj@sitca-ve.com>
2024-08-14 17:37:45 -0400

    MOD: metodo para obtener un bookmark

--- repository/BookmarkRepository.go
+++ repository/BookmarkRepository.go
@@ -1,1 +1,21 @@
 package repository
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"manga-bookmarker-backend/constants"
+	"manga-bookmarker-backend/models"
+)
+
+func FindBookmarkByAny(key, value string) (bookmark models.Bookmark, errorType int, err error) {
+	err = DB.Collection("bookmarks").FindOne(context.TODO(), bson.M{key: value}).Decode(&bookmark)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return bookmark, constants.NoDocumentFound, nil
+		} else {
+			return bookmark, constants.Other, err
+		}
+	}
+	return bookmark, constants.NoError, nil
+}
