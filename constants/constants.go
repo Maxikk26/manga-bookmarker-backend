80cc5fe2578c7358f38512072cf842d6e1f8e823
Maximiliano Bogoljubskij <mbogoljubsklj@sitca-ve.com>
2024-08-14 17:03:12 -0400

    ADD: dto del scrapper y constante de tipo de errores

---
+++ constants/constants.go
@@ -0,0 +1,7 @@
+package constants
+
+const (
+	NoError         = -1
+	Other           = 0
+	NoDocumentFound = 1
+)
---
+++ dtos/MangaDto.go
@@ -0,0 +1,7 @@
+package dtos
+
+type MangaScrapperData struct {
+	Name          string `json:"name"`
+	Cover         string `json:"cover"`
+	TotalChapters string `json:"totalChapters"`
+}
