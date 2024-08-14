50262a3e69cd66a57872627550db72291ba40705
Maximiliano Bogoljubskij <mbogoljubsklj@sitca-ve.com>
2024-08-14 17:04:02 -0400

    MOD: ajuste del servicio de scrapping para enviar data resultante por un channel

--- services/ScrapperService.go
+++ services/ScrapperService.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"github.com/gocolly/colly/v2"
 	"log"
+	"manga-bookmarker-backend/dtos"
 	"net/url"
 	"strings"
-	"sync"
 	"time"
 )
 
@@ -29,8 +29,10 @@ func obtainDomainGlob(urlStr string) (string, error) {
 	return modifiedDNS, nil
 }
 
-func ScrapperService(url string, wg *sync.WaitGroup) {
+func ScrapperService(url string, ch chan<- dtos.MangaScrapperData) {
 	start := time.Now()
+
+	var data dtos.MangaScrapperData
 
 	c := colly.NewCollector(
 		colly.Async(true),              // Enable asynchronous requests
@@ -56,24 +58,30 @@ func ScrapperService(url string, wg *sync.WaitGroup) {
 
 	// div element with class story-info-right to obtain manga title
 	c.OnHTML("div.story-info-right h1", func(e *colly.HTMLElement) {
+		data.Name = e.Text
 		fmt.Println("title: ", e.Text)
 	})
 
 	// span element of div parent to obtain image src of manga
 	c.OnHTML("div.story-info-left span.info-image img", func(e *colly.HTMLElement) {
 		fmt.Println("image url:", e.Attr("src"))
+		data.Cover = e.Attr("src")
 	})
 
 	// Process only the first li element within ul.row-content-chapter
 	c.OnHTML("ul.row-content-chapter li:first-child", func(e *colly.HTMLElement) {
 		parts := strings.Fields(e.Text)
+		fmt.Println("parts: ", parts)
 		if len(parts) > 1 {
 			result := parts[1]
+			data.TotalChapters = result
 			fmt.Println("result:", result)
 		} else {
 			fmt.Println("The input string does not contain enough parts.")
 		}
 	})
+
+	//TODO obtain and transform the date of update [1 Day ago, X hour ago, (month day, year)]
 
 	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
@@ -81,12 +89,16 @@ func ScrapperService(url string, wg *sync.WaitGroup) {
 	})
 
 	// Start scraping on url provided
-	c.Visit("https://chapmanganato.to/manga-aa951883")
+	c.Visit(url)
 
 	// Wait for all async tasks to complete
 	c.Wait()
 
+	ch <- data
+
 	elapsed := time.Since(start) // Calculate the elapsed time
 	fmt.Printf("Execution time: %s\n", elapsed)
-	defer wg.Done()
+	fmt.Println(fmt.Sprintf("%+v", data))
+	//defer wg.Done()
+	//return data
 }
