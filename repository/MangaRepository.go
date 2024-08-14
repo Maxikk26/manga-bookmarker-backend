7c8a5b08ab45786de87a738a86c420dbfee8aff3
Maximiliano Bogoljubskij <mbogoljubsklj@sitca-ve.com>
2024-08-14 17:05:38 -0400

    ADD: validar si manga existe, si no existe hacer scrapping e insertarlo

--- repository/MangaRepository.go
+++ repository/MangaRepository.go
@@ -1,1 +1,30 @@
 package repository
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"manga-bookmarker-backend/constants"
+	"manga-bookmarker-backend/models"
+)
+
+func CreateManga(data models.Manga) (id interface{}, err error) {
+	// Insert the new user into the collection
+	res, err := DB.Collection("mangas").InsertOne(context.TODO(), data)
+	if err != nil {
+		return nil, err
+	}
+	return res.InsertedID, nil
+}
+
+func FindMangaByAny(key, value string) (manga models.Manga, errorType int, err error) {
+	err = DB.Collection("users").FindOne(context.TODO(), bson.M{key: value}).Decode(&manga)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return manga, constants.NoDocumentFound, nil
+		} else {
+			return manga, constants.Other, err
+		}
+	}
+	return manga, constants.NoError, nil
+}
--- services/BookmarkService.go
+++ services/BookmarkService.go
@@ -3,13 +3,13 @@ package services
 import (
 	"errors"
 	"fmt"
+	"manga-bookmarker-backend/constants"
 	"manga-bookmarker-backend/dtos"
+	"manga-bookmarker-backend/repository"
 	"strings"
 )
 
 func CreateBookmark(data dtos.CreateBookmark) error {
-	fmt.Println(fmt.Sprintf("%+v", data))
-	//TODO obtain id of manga in URL
 
 	// Find the index of "manga-"
 	prefix := "manga-"
@@ -20,18 +20,56 @@ func CreateBookmark(data dtos.CreateBookmark) error {
 	}
 
 	// Extract the substring after "manga-"
-	mangaID := data.Url[idx+len(prefix):]
+	mangaIdentifier := data.Url[idx+len(prefix):]
 
 	// Check if there's a "/" in the extracted ID and handle it accordingly
-	if slashIdx := strings.Index(mangaID, "/"); slashIdx != -1 {
-		mangaID = mangaID[:slashIdx] // Exclude the part after "/"
+	if slashIdx := strings.Index(mangaIdentifier, "/"); slashIdx != -1 {
+		mangaIdentifier = mangaIdentifier[:slashIdx] // Exclude the part after "/"
 	}
 
-	fmt.Println("Manga ID:", mangaID)
+	fmt.Println("mangaIdentifier", mangaIdentifier)
 
-	//TODO check if id of manga exists
+	manga, errorType, err := repository.FindMangaByAny("identifier", mangaIdentifier)
+	if err != nil {
+		fmt.Println("Error obtaining manga:", err.Error())
+		return errors.New("No se encontró el manga especificado")
+	}
 
-	//TODO create manga if not exists and obtain ObjectId
+	fmt.Println("manga", manga)
+
+	if errorType != constants.NoDocumentFound {
+		fmt.Println("Error type NoDocumentFound after checking for err variable:", err.Error())
+		return errors.New("Ocurrio un error buscando el manga")
+	}
+
+	//TODO scrap manga info to poblate manga struct
+	//Channel to push the result of the scrapper to a struct so we can do nexts steps after we have the data
+	ch := make(chan dtos.MangaScrapperData)
+
+	//Call scraooer service as a goroutine
+	go ScrapperService(data.Url, ch)
+
+	mangaData := <-ch
+
+	// Use dto-mapper to map the data to the struct
+	err = mapper.Map(&manga, &mangaData)
+	if err != nil {
+		fmt.Println("Error mapping data:", err)
+		return err
+	}
+
+	//Set de manga identifier
+	manga.Identifier = mangaIdentifier
+
+	fmt.Println(fmt.Sprintf("%+v", manga))
+
+	id, err := repository.CreateManga(manga)
+	if err != nil {
+		fmt.Println("Error creating manga: ", err.Error())
+		return errors.New("Ocurrio un error creando el manga")
+	}
+
+	fmt.Println(id)
 
 	//TODO check if the bookmark for that manga does not exists
 
