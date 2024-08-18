b7a0f460d9db5d4b7d256e59e154215e637bf281
Maximiliano Bogoljubskij <mbogoljubsklj@sitca-ve.com>
2024-08-18 16:16:18 -0400

    ADD: comentarios de constantes

--- constants/constants.go
+++ constants/constants.go
@@ -1,10 +1,12 @@
 package constants
 
 const (
+	//Mongo error handling
 	NoError         = -1
 	Other           = 0
 	NoDocumentFound = 1
 
+	//Bookmark status
 	Reading        = 1
 	PlanningToRead = 2
 	OnHold         = 3
